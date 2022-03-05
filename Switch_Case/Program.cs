e42384a88507fe39e8f2a7c209ee7361d1dd9dbf
Nazlı <nazlisn123@gmail.com>
2022-03-06 00:08:42 +0300

    Switch Case practice completed

---
+++ Switch_Case/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Swicth_Case
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak");
+                    break;
+                case 2:
+                    Console.WriteLine("Subat");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart");
+                    break;
+                case 4:
+                    Console.WriteLine("Mayis");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kis ayindasiniz");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Ilk Bahar ayindasiniz");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz  ayindasiniz");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Son Bahar  ayindasiniz");
+                    break;
+            }
+        }
+    }
+}
