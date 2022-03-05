e547bac482fad510ad3b4cbef0695dcf79512fc9
Nazlı <nazlisn123@gmail.com>
2022-03-05 21:43:31 +0300

    Type conversion  pratice completed

---
+++ Type_Conversions/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Type_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- Implict Conversion----");
+            byte a = 5;
+            sbyte b = 30;
+            short c = 10;
+
+            int d = a + b + c;
+            Console.WriteLine(d);
+
+            long h = d;
+            Console.WriteLine(h);
+
+            float i = h;
+            Console.WriteLine(i);
+
+            string e = "zikriye";
+            char f = 'k';
+            object g = e + f + d;
+            Console.WriteLine(g);
+
+            Console.WriteLine("---- Explicit Conversion----");
+            int x = 4;
+            byte y = (byte)x;
+            Console.WriteLine(y);
+
+            int z = 100;
+            byte t = (byte)z;
+            Console.WriteLine(t);
+
+            float w = 10.3f;
+            byte v = (byte)w;
+            Console.WriteLine(v);
+
+            int xx = 6;
+            string yy = xx.ToString();
+
+            Console.WriteLine(yy);
+
+            string zz = 12.5f.ToString();
+            Console.WriteLine(zz);
+
+            string s1 = "10", s2 = "20";
+            int sayi1, sayi2;
+            int Toplam;
+
+            sayi1 = Convert.ToInt32(s1);
+            sayi2 = Convert.ToInt32(s2);
+
+            Toplam = sayi1 + sayi2;
+            Console.WriteLine(Toplam);
+        }
+    }
+}
